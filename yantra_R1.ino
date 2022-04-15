ee3662f45198295c6c3bc6a51c1b93dcf312d449
prabhav <64297193+prabhavms@users.noreply.github.com>
2022-04-15 19:08:34 +0530

    Update and rename yantra.ino to yantra_R1.ino

--- yantra_R1.ino
+++ yantra_R1.ino
@@ -1,4 +1,5 @@
-// Line Follower
+// FOR THE ROBOT R1
+
 
 #define enA 5//Enable1 L293 Pin enA 
 #define in1 6 //Motor1  L293 Pin in1 
@@ -6,8 +7,8 @@
 #define in3 9 //Motor2  L293 Pin in1 
 #define in4 10 //Motor2  L293 Pin in1 
 #define enB 8 //Enable2 L293 Pin enB 
-#define R_S 4//ir sensor Right
-#define L_S 2 //ir sensor Left
+#define R_S 4//colour sensor Right
+#define L_S 4 //colour sensor Left
 void setup(){ 
 pinMode(R_S, INPUT); 
 pinMode(L_S, INPUT); 
@@ -21,23 +22,20 @@ digitalWrite(enA, HIGH);
 digitalWrite(enB, HIGH); 
 delay(1000);
 }
+
+
 void loop(){  
-if((digitalRead(R_S) == 0)&&(digitalRead(L_S) == 0)){forward();}     //if Right Sensor and Left Sensor are at White color then it will call forword function
-if((digitalRead(R_S) == 1)&&(digitalRead(L_S) == 0)){turnRight();}  //if Right Sensor is Black and Left Sensor is White then it will call turn Right function  
-if((digitalRead(R_S) == 0)&&(digitalRead(L_S) == 1)){turnLeft();}   //if Right Sensor is White and Left Sensor is Black then it will call turn Left function
-if((digitalRead(R_S) == 1)&&(digitalRead(L_S) == 1)){Stop();}      //if Right Sensor and Left Sensor are at Black color then it will call Stop function
+if((digitalRead(R_S) == 2)&&(digitalRead(L_S) == 3)){forward();}      //if Right Sensor is Green and Left Sensor is Yellow then it will call forward function.
+if((digitalRead(R_S) == 1)&&(digitalRead(L_S) == 1)){forward();}    //if Right Sensor and Left Sensor is Black then it will call forward function.
+if((digitalRead(R_S) == 5)&&(digitalRead(L_S) == 2)){forward();}    //if Right Sensor is Red and Left Sensor is Green then it will call forward function.  
+if((digitalRead(R_S) == 4)&&(digitalRead(L_S) == 4)){turnLeft();}   //if Right Sensor and Left Sensor are at Blue color then it will call turn Left function.
+if((digitalRead(R_S) == 0)&&(digitalRead(L_S) == 0)){Stop();}      //if Right Sensor and Left Sensor are at White color then it will call Stop function
 }
 void forward(){  //forword
 digitalWrite(in1, HIGH); //Right Motor forword Pin 
 digitalWrite(in2, LOW);  //Right Motor backword Pin 
 digitalWrite(in3, LOW);  //Left Motor backword Pin 
 digitalWrite(in4, HIGH); //Left Motor forword Pin 
-}
-void turnRight(){ //turnRight
-digitalWrite(in1, LOW);  //Right Motor forword Pin 
-digitalWrite(in2, HIGH); //Right Motor backword Pin  
-digitalWrite(in3, LOW);  //Left Motor backword Pin 
- digitalWrite(in4, HIGH); //Left Motor forword Pin 
 }
 void turnLeft(){ //turnLeft
 digitalWrite(in1, HIGH); //Right Motor forword Pin 
